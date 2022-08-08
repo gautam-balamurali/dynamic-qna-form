3a824c8d24f32f7c8f28f7c0bd648fc018b127c4
Gautam Balamurali <gtmbalamurali@gmail.com>
2022-08-08 05:49:47 +0530

    Development init

--- src/app/dependency-modules/dependency-modules.module.ts
+++ src/app/dependency-modules/dependency-modules.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
+const Dependencies = [
+  MatSelectModule,
+  MatFormFieldModule,
+  MatInputModule,
+  FormsModule,
+  ReactiveFormsModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, Dependencies],
+  exports: [Dependencies],
 })
-export class DependencyModulesModule { }
+export class DependencyModulesModule {}
